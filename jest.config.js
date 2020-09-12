f5185874dc8e73ef329dc7e7258f183c7db58746
olympiawoj <owojcik7@gmail.com>
2020-09-12 11:06:55 -0400

    4. Configure Jest's Test Environment for Testing Node or Browser Code

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // testEnvironment: 'jest-environment-node', // ensures tests are running in a node environment
+    testEnvironment: 'jest-environment-jsdom' // ensures tests are running in browser simulated environment
+}
--- src/shared/__tests__/utils.js
+++ src/shared/__tests__/utils.js
@@ -1,7 +1,12 @@
 // 1) Import what we are testing
 import {getFormattedValue} from '../utils'
 
+// 2) Describe what we are testing
 test('formats the value', ()=>{
+    // 3) Assertion
     expect(getFormattedValue('1234.0')).toBe('1,234.0')
 })
+
+
+// console.log(window)
 
