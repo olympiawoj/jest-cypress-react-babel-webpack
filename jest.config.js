eb41ff552459d8506204bcabf343b505e79625d4
olympiawoj <owojcik7@gmail.com>
2020-09-12 16:16:53 -0400

    9. Support Custom Module Resolution with Jest moduleDirectories

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
+const path =  require('path') // resolvers to directory name + src, so exact same place we have in webpack config
+// node modules will be treated like omduoel
+
 module.exports = {
     // testEnvironment: 'jest-environment-node', // ensures tests are running in a node environment
     testEnvironment: 'jest-environment-jsdom', // ensures tests are running in browser simulated environment
+    moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
     moduleNameMapper: {
         // match all imports for .module.css bc those will have an object w styles applied, in particular .auto-scaling-text
         '\\.module\\.css$':'identity-obj-proxy', // returns a string for path accessed 
