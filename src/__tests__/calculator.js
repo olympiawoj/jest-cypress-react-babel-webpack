acdf76d03a0034731c5796590d43b8451355539f
olympiawoj <owojcik7@gmail.com>
2020-09-15 21:36:39 -0400

    10. Configure Jest to Run Setup for All Tests with Jest setupFilesAfterEnv

--- src/__tests__/calculator.js
+++ src/__tests__/calculator.js
@@ -1,8 +1,19 @@
+// import * as jestDOM from '@testing-library/jest-dom'
+import '@testing-library/jest-dom/extend-expect' // does expect.extend(jestDOM) below for us
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import Calculator from '../calculator'
 
+// expect.extend(jestDOM) // jestDOM object is a bunch of jest extensions, adds new assertions for us
 
-test('renders', ()=>{
-    render(<Calculator/>)
+test('the clear button switches from AC to C when there is an entry', ()=>{
+    const {getByText} = render(<Calculator/>)
+    const clearButton = getByText('AC')
+
+    fireEvent.click(getByText(/3/))
+    expect(clearButton).toHaveTextContent('C')
+    fireEvent.click(clearButton)
+    expect(clearButton).toHaveTextContent('AC')
 })
+
+
