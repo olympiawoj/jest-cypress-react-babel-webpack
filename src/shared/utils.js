780b1436b61fe3df9d3f1532ec78916df287e17a
olympiawoj <owojcik7@gmail.com>
2020-09-19 15:01:45 -0400

    15. Analyze Jest Code Coverage Reports

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -7,6 +7,7 @@ function getFormattedValue(value, language = 'en-US') {
   // Add back missing .0 in e.g. 12.0
   const match = value.match(/\.\d*?(0*)$/)
 
+  /* istanbul ignore next - says this isn't important so ignore this part of coverage*/
   if (match) {
     formattedValue += /[1-9]/.test(match[0]) ? match[1] : match[0]
   }
