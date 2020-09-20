66fcd2a1b1459a01cfbb0f0ef3f40a16c6c83e6a
olympiawoj <owojcik7@gmail.com>
2020-09-20 08:59:10 -0400

    16. Set a Code Coverage Threshold in Jest to Maintain Code Coverage Levels

--- jest.config.js
+++ jest.config.js
@@ -13,4 +13,18 @@ module.exports = {
     setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'], // files jest will run after setting up jest testing environment
     snapshotSerializers: ['jest-emotion'],
     collectCoverageFrom: ['**/src/**/*.js'], // array of globs - anything in src directory that ends in .js, collect coverage
+    coverageThreshold: {
+        global: {
+            statements: 31,
+            branches: 18,
+            functions: 29,
+            lines: 29,
+        },
+        './src/shared/utils.js':{
+            statements: 100,
+            branches: 80,
+            functions: 100,
+            lines: 100,
+        }
+    }
 }
